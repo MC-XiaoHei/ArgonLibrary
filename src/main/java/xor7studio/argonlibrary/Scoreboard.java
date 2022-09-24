ef0d8bfe603fc943d5409b2ded4818f31890906e
Administrator <xhwyx.tcloud@qq.com>
2022-09-24 08:47:37 +0800

    220924

--- src/main/java/xor7studio/argonlibrary/Scoreboard.java
+++ src/main/java/xor7studio/argonlibrary/Scoreboard.java
@@ -1,22 +1,18 @@
 package xor7studio.argonlibrary;
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.scoreboard.Scoreboard;
-import org.lwjgl.system.CallbackI;
 import xor7studio.argonlibrary.scoreboard.ScoreboardHandler;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-public class ScoreboardManager {
+public class Scoreboard {
     private List<String> objectives;
     private Integer size;
     private String name;
     private ScoreboardHandler scoreboardHandler;
-    public ScoreboardManager(String name){init(0,name);}
-    public ScoreboardManager(int size, String name){init(size,name);}
+    public Scoreboard(String name){init(0,name);}
+    public Scoreboard(int size, String name){init(size,name);}
     private void init(int size,String name){
         objectives = new ArrayList<>();
         for(int i=0;i<size;i++)objectives.add("");
@@ -30,5 +26,10 @@ public class ScoreboardManager {
         List<String> newList=new ArrayList<>();
         for(int i=0;i<size;i++)newList.add(objectives.get(i));
         objectives=newList;
+    }
+    public void setLine(int line,String data){
+        if(line>size)
+            resize(line);
+        objectives.set(line,data);
     }
 }
